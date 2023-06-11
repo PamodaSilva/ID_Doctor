290eca936458a2df6b73f924e5fc1611ae4595d3
pamodasilva99 <96534@fhss.sjp.ac.lk>
2023-06-11 12:54:37 +0530

    Create Splash Screen

--- app/screens/SplashScreen.js
+++ app/screens/SplashScreen.js
@@ -1,31 +1,64 @@
 /* eslint-disable prettier/prettier */
-import React, { useEffect } from 'react';
-import { View, Image, StyleSheet } from 'react-native';
+import React, { Component } from 'react';
+import { View, Image, StyleSheet} from 'react-native';
+import Home from '../screens/home';
 
-const SplashScreen = () => {
-    useEffect(() => {
-      // Perform any necessary tasks while the splash screen is displayed, such as data loading or app initialization.
-      // You can use the `useEffect` hook to define the necessary logic.
-      // Once you're done, navigate to the next screen using a navigation library like React Navigation.
-    }, []);
-  
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false, // Flag to determine if the necessary tasks are completed
+    };
+  }
+
+  componentDidMount() {
+    // Perform any necessary tasks while the splash screen is displayed, such as data loading or app initialization.
+    // You can use the `componentDidMount` lifecycle method to define the necessary logic.
+    // Once you're done, update the state to indicate that the tasks are completed.
+
+    // Simulating a delay of 3 seconds for demonstration purposes
+    setTimeout(() => {
+      this.setState({ isLoaded: true });
+    }, 3000);
+  }
+
+  render() {
+    const { isLoaded } = this.state;
+
+    if (!isLoaded) {
+      return (
+        <View style={styles.splashContainer}>
+          <Image source={require('../assets/Doc.png')} style={styles.splashImage} />
+        </View>
+      );
+    }
+
     return (
       <View style={styles.container}>
-        <Image source={require('./path/to/your/splashImage.png')} style={styles.image} />
+        <>
+        <Home/>
+        </>
       </View>
     );
-  };
+  }
+}
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    image: {
-      width: 200,
-      height: 200,
-      resizeMode: 'contain',
-    },
-  });
-  
+const styles = StyleSheet.create({
+  splashContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  splashImage: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+});
