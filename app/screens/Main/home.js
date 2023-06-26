93a23c9a2ccfbc638fffe588f68a07653b53e2a7
pamodasilva99 <96534@fhss.sjp.ac.lk>
2023-06-26 14:52:52 +0530

    Add links, get more details about Diseases.

--- app/screens/Main/home.js
+++ app/screens/Main/home.js
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import { View, ScrollView, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { View, ScrollView, Text, StyleSheet, Image, TouchableOpacity, Linking } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 const HomePage = () => {
@@ -16,6 +16,80 @@ const HomePage = () => {
 
   const handleProfile = () => {
     navigation.navigate('Profile');
+  };
+
+  const handleText1Click = () => {
+    const websiteUrl = 'https://en.wikipedia.org/wiki/Common_cold';
+    Linking.openURL(websiteUrl);
+  };
+
+  const handleText2Click = () => {
+    const websiteUrl = 'https://www.cdc.gov/flu/symptoms/symptoms.htm';
+    Linking.openURL(websiteUrl);
+  };
+  const handleText3Click = () => {
+    const websiteUrl = 'https://en.wikipedia.org/wiki/COVID-19';
+    Linking.openURL(websiteUrl);
+  };
+
+  const handleText4Click = () => {
+    const websiteUrl = 'https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwiyj9qyvOD_AhUqmGYCHZSDCpgYABAAGgJzbQ&ohost=www.google.com&cid=CAESa-D2kCKfSVa-VBAq1wr7Ll8rY2DD8N-t9V3DkwDwf_QuX0VMmZnOWC72JmBbVS7cryvOqVcuDjm44mXbV2DTaUqKXWJjkDwjldfYomQry3MCBytGIMOoXUC4QsSdzh3R5M7kqkVX10cI_pL9&sig=AOD64_0ynu1K1fF8J5fkzurTD8Pc9iZWfg&q&adurl&ved=2ahUKEwiw6NKyvOD_AhUY2TgGHWW5C4sQ0Qx6BAgGEAEd';
+    Linking.openURL(websiteUrl);
+  };
+
+  const handleText5Click = () => {
+    const websiteUrl = 'https://en.wikipedia.org/wiki/Hepatitis';
+    Linking.openURL(websiteUrl);
+  };
+
+  const handleText6Click = () => {
+    const websiteUrl = 'https://dndi.org/diseases/dengue/facts/?gclid=Cj0KCQjw7uSkBhDGARIsAMCZNJtr7-n0vUiaxYrB67zu3tKLlyB4Lbx6G_T8WsE8gON5n3QoD8S-LEcaAphJEALw_wcB';
+    Linking.openURL(websiteUrl);
+  };
+
+  const handleText7Click = () => {
+    const websiteUrl = 'https://en.wikipedia.org/wiki/Avian_influenza';
+    Linking.openURL(websiteUrl);
+  };
+
+  const handleText8Click = () => {
+    const websiteUrl = 'https://www.who.int/health-topics/severe-acute-respiratory-syndrome#tab=tab_1';
+    Linking.openURL(websiteUrl);
+  };
+
+  const handleText9Click = () => {
+    const websiteUrl = 'https://www.who.int/news-room/fact-sheets/detail/zika-virus?gclid=Cj0KCQjw7uSkBhDGARIsAMCZNJucKpASPbuvpceoRR35QPL_ofUdqqlw752uFgl-M-64SL2N899qFCAaAqSbEALw_wcB';
+    Linking.openURL(websiteUrl);
+  };
+
+  const handleText10Click = () => {
+    const websiteUrl = 'https://www.who.int/news-room/fact-sheets/detail/nipah-virus';
+    Linking.openURL(websiteUrl);
+  };
+
+  const handleText11Click = () => {
+    const websiteUrl = 'https://www.who.int/news-room/questions-and-answers/item/malaria?gclid=Cj0KCQjw7uSkBhDGARIsAMCZNJsE9YSMyYlK45IADDyl3XvCzjp2Nh5dtGjlhi_iKlP2Nr7U0AnQ1eIaAgNHEALw_wcB';
+    Linking.openURL(websiteUrl);
+  };
+
+  const handleText12Click = () => {
+    const websiteUrl = 'https://www.who.int/news-room/fact-sheets/detail/cholera?gclid=Cj0KCQjw7uSkBhDGARIsAMCZNJt2LWUy_a4V5CeZ8MjX_gSr-iXnvYg3hzDGjI35drNr8HAgGD2DL64aAvq6EALw_wcB';
+    Linking.openURL(websiteUrl);
+  };
+
+  const handleText13Click = () => {
+    const websiteUrl = 'https://www.who.int/news-room/fact-sheets/detail/measles?gclid=Cj0KCQjw7uSkBhDGARIsAMCZNJsps18OrTROg4gE54_JmHuPmgTmzeaHwjIescjnqctYaB7BRNl7hX8aArKAEALw_wcB';
+    Linking.openURL(websiteUrl);
+  };
+
+  const handleText14Click = () => {
+    const websiteUrl = 'https://www.gavi.org/vaccineswork/next-pandemic/yellow-fever?gclid=Cj0KCQjw7uSkBhDGARIsAMCZNJuxVzIKf8Bu5hPzAwg_xSKYoHSU60PmI_KfTAfrTlqX5Jkejes3vlcaAidOEALw_wcB';
+    Linking.openURL(websiteUrl);
+  };
+
+  const handleText15Click = () => {
+    const websiteUrl = 'https://www.mayoclinic.org/diseases-conditions/flu/symptoms-causes/syc-20351719';
+    Linking.openURL(websiteUrl);
   };
 
   return (
@@ -51,24 +125,25 @@ const HomePage = () => {
           <Text style={styles.sectionHeading3}>These are the Infectious Disease Predicted by this app.</Text>
           <View style={styles.section3View}>
             <Text style={styles.sectionText3}>
-              ● Common cold {'\n'}
-              ● Normal flu  {'\n'}
-              ● Covid-19 {'\n'}
-              ● Monkeypox {'\n'}
-              ● Hepatitis (A,B,C)
+              <Text onPress={handleText1Click}>  ● Common cold {'\n'} </Text>
+              <Text  onPress={handleText2Click}> ● Normal flu {'\n'} </Text>
+              <Text onPress={handleText3Click}> ● Covid-19 {'\n'} </Text>
+              <Text onPress={handleText4Click}> ● Monkeypox {'\n'} </Text>
+              <Text onPress={handleText5Click}> ● Hepatitis {'\n'} </Text>
+              <Text onPress={handleText6Click}> ● Dengue  </Text>
             </Text>
             <Text style={styles.sectionText3}>
-            ● Bird Flu {'\n'}
-            ● SARS {'\n'}
-            ● Zika {'\n'}
-            ● Nipah {'\n'}
-            ● Malaria {'\n'}
-            ● Dengue {'\n'}
-            ● Cholera {'\n'}
-            ● Measles {'\n'}
-            ● Yellow Flu{'\n'}
-            ● Influenza
+              <Text onPress={handleText7Click}>  ● Bird Flu {'\n'} </Text>
+              <Text onPress={handleText8Click}> ● SARS {'\n'} </Text>
+              <Text onPress={handleText9Click}> ● Zika {'\n'} </Text>
+              <Text onPress={handleText10Click}> ● Nipah {'\n'} </Text>
+              <Text onPress={handleText11Click}> ● Malaria {'\n'} </Text>
+              <Text onPress={handleText12Click}> ● Cholera {'\n'} </Text>
+              <Text onPress={handleText13Click}> ● Measles {'\n'} </Text>
+              <Text onPress={handleText14Click}> ● Yellow Flu {'\n'} </Text>
+              <Text onPress={handleText15Click}> ● Influenza </Text>
             </Text>
+            <Text style={styles.LastText}>Do you want know more details about infectious diseases, Click on Disease Name. </Text>
           </View>
           <Image source={require('../../assets/Chat.jpg')} style={styles.Image3} />
         </View>
@@ -193,9 +268,9 @@ const styles = StyleSheet.create({
     marginTop: -50,
   },
   sectionText3: {
-    fontSize: 16,
+    fontSize: 18.5,
     marginBottom: 20,
-    paddingLeft: 30,
+    paddingLeft: 25,
     color: 'black',
   },
   sectionHeading3: {
@@ -205,6 +280,12 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: 'black',
     fontStyle: 'italic',
+  },
+  LastText: {
+    color: 'brown',
+    fontSize: 15,
+    textAlign: 'justify',
+    marginBottom: 10,
   },
 
 });
